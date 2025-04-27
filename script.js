cc001096993bcee93a15232f51a46ff3b0c32991
isha3011 <84734703+isha3011@users.noreply.github.com>
2025-04-27 23:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,15 @@ $(document).ready(function(){
     });
   });
   
+//   sidebar 
+const hamburger = document.getElementById('hamburger');
+  const sidebar = document.getElementById('sidebar');
+  const closeBtn = document.getElementById('close-btn');
+
+  hamburger.addEventListener('click', () => {
+    sidebar.classList.add('active');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+  });
